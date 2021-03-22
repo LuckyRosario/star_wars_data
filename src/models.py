78c9c4c0d25c426e061e4b4c520981f71de0a442
LuckyRosario <p.rosario684@gmail.com>
2021-03-22 03:57:52 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -8,23 +8,71 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    date = Column(String(250), nullable=False)
+    text = Column(String(250), nullable=False)
+    comments = Column(String(250), nullable=False)
+    user = relationship(User)
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    date = Column(String(250), nullable=False)
+    text = Column(String(250), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    user = relationship(User)
+    post = relationship(Post)
+
+class Entertainment(Base):
+    __tablename__ = 'entertainment'
+    id = Column(Integer, primary_key=True)
+    media_type = Column(String(250), nullable=False)
+    rating = Column(String(250), nullable=False)
+    title = Column(String(250), nullable=False)
+    
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    character_name = Column(String(250), nullable=False)
+    height = Column(String(250), nullable=False)
+    eyecolor = Column(String(250), nullable=False)
+    entertainment_id = Column(Integer, ForeignKey('entertainment.id'))
+    entertainment = relationship(Entertainment)
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    planet_name = Column(String(250), nullable=False)
+    gravity = Column(String(250), nullable=False)
+    population = Column(String(250), nullable=False)
+    climate = Column(String(250), nullable=False)
+    character_id = Column(Integer, ForeignKey('character.id'))
+    character = relationship(Character)
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    character_id = Column(Integer, ForeignKey('character.id'))
+    media_id = Column(Integer, ForeignKey('media.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    character = relationship(Character)
+    planet = relationship(Planet)
+    user = relationship(User)
+    post = relationship(Post)
+    entertainment = relationship(Entertainment)
 
     def to_dict(self):
         return {}
